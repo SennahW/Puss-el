f5a6288311789b86591a32cc3e2aea3fe16939db
GG <48587537+Hoirshou@users.noreply.github.com>
2019-03-26 13:36:59 +0100

    Platforms movement

--- Puss-el/Assets/Buttons/Buttons.cs
+++ Puss-el/Assets/Buttons/Buttons.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class b : MonoBehaviour
+public class buttons : MonoBehaviour
 {
+    float speedLeft = 10f;
+    float speedRight = -10f;
+    float dirX, myspeed = 4f;
+    bool moveright = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,23 @@ public class b : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x > speedLeft )
+        {
+            moveright = false;
+
+        }
+        else if (transform.position.x < speedRight )
+        {
+            moveright = true;
+        }
+
+        if (moveright == true)
+        {
+            transform.position = new Vector2(transform.position.x + myspeed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - myspeed * Time.deltaTime, transform.position.y);
+        }
     }
 }
