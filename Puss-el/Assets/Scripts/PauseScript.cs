13fdd74e6eb47acec1372f44843711661ffa5ce6
GG <48587537+Hoirshou@users.noreply.github.com>
2019-05-14 13:38:41 +0200

    gg

---
+++ Puss-el/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+
+{
+    public bool isPaused;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    public void ActivateMenu()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void DeactivateMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ToLevelSelect()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Resume()
+    {
+        isPaused = !isPaused;
+    }
+}
